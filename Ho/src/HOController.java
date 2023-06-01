069bb1f488841150c6d12cd52994ec53b55a8228
Oumayma-O <oumayma.ouerfelli@insat.ucar.tn>
2023-06-01 04:10:44 +0100

    Initial commit

--- Ho/src/HOController.java
+++ Ho/src/HOController.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.sql.*;
 
 import com.rabbitmq.client.*;
@@ -15,7 +17,7 @@ public class HOController {
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "";
 
-    private Connection rabbitmqConnection;
+    private java.sql.Connection dbconnection;
     private Channel channel;
     private PreparedStatement insertIntoHOStmt;
 
@@ -28,45 +30,95 @@ public class HOController {
     }
 
     public void startSync() throws Exception {
-        // Consume messages from BO1 queue
-        channel.basicConsume(BO1_QUEUE_NAME, true, (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
 
-                String region = delivery.getProperties().getHeaders().get("region").toString();
-                String product = delivery.getProperties().getHeaders().get("product").toString();
-                int qty = Integer.parseInt(delivery.getProperties().getHeaders().get("qty").toString());
-                float cost = Float.parseFloat(delivery.getProperties().getHeaders().get("cost").toString());
-                double amt = Double.parseDouble(delivery.getProperties().getHeaders().get("amt").toString());
-                float tax = Float.parseFloat(delivery.getProperties().getHeaders().get("tax").toString());
-                double total = Double.parseDouble(delivery.getProperties().getHeaders().get("total").toString());
+        // Consume messages from BO1 queue and insert into HO database
+        Consumer consumer_1 = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(message);
+                String[] data = message.split(",");
+
+
                 try {
-                    insertSale( region, product, qty, cost, amt, tax, total);
+                    Date date = (Date) properties.getHeaders().get("date");
+
+                    String region = properties.getHeaders().get("region").toString();
+                    String product = properties.getHeaders().get("product").toString();
+                    int qty = Integer.parseInt(properties.getHeaders().get("qty").toString());
+                    float cost = Float.parseFloat(properties.getHeaders().get("cost").toString());
+                    double amt = Double.parseDouble(properties.getHeaders().get("amt").toString());
+                    float tax = Float.parseFloat(properties.getHeaders().get("tax").toString());
+                    double total = Double.parseDouble(properties.getHeaders().get("total").toString());
+
+                    insertIntoHOStmt.setDate(1,date );
+                    insertIntoHOStmt.setString(2, region);
+                    insertIntoHOStmt.setString(3, product);
+                    insertIntoHOStmt.setInt(4, qty);
+                    insertIntoHOStmt.setFloat(5, cost);
+                    insertIntoHOStmt.setDouble(6, amt);
+                    insertIntoHOStmt.setFloat(7, tax);
+                    insertIntoHOStmt.setDouble(8, total);
+                    System.out.println(insertIntoHOStmt);
+                    insertIntoHOStmt.executeUpdate();
+
+                    insertIntoHOStmt.executeUpdate();
+                    insertIntoHOStmt.close();
+                    System.out.println(message);
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
+            }
+        };
 
-        }, consumerTag -> {});
 
-        // Consume messages from BO2 queue
-        channel.basicConsume(BO2_QUEUE_NAME, true, (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
+        // Consume messages from BO2 queue and insert into HO database
+        Consumer consumer_2 = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(message);
+                String[] data = message.split(",");
 
-                String region = delivery.getProperties().getHeaders().get("region").toString();
-                String product = delivery.getProperties().getHeaders().get("product").toString();
-                int qty = Integer.parseInt(delivery.getProperties().getHeaders().get("qty").toString());
-                float cost = Float.parseFloat(delivery.getProperties().getHeaders().get("cost").toString());
-                double amt = Double.parseDouble(delivery.getProperties().getHeaders().get("amt").toString());
-                float tax = Float.parseFloat(delivery.getProperties().getHeaders().get("tax").toString());
-                double total = Double.parseDouble(delivery.getProperties().getHeaders().get("total").toString());
+
                 try {
-                    insertSale( region, product, qty, cost, amt, tax, total);
+                    Date date = (Date) properties.getHeaders().get("date");
+
+                    String region = properties.getHeaders().get("region").toString();
+                    String product = properties.getHeaders().get("product").toString();
+                    int qty = Integer.parseInt(properties.getHeaders().get("qty").toString());
+                    float cost = Float.parseFloat(properties.getHeaders().get("cost").toString());
+                    double amt = Double.parseDouble(properties.getHeaders().get("amt").toString());
+                    float tax = Float.parseFloat(properties.getHeaders().get("tax").toString());
+                    double total = Double.parseDouble(properties.getHeaders().get("total").toString());
+
+                    insertIntoHOStmt.setDate(1,date );
+                    insertIntoHOStmt.setString(2, region);
+                    insertIntoHOStmt.setString(3, product);
+                    insertIntoHOStmt.setInt(4, qty);
+                    insertIntoHOStmt.setFloat(5, cost);
+                    insertIntoHOStmt.setDouble(6, amt);
+                    insertIntoHOStmt.setFloat(7, tax);
+                    insertIntoHOStmt.setDouble(8, total);
+                    insertIntoHOStmt.executeUpdate();
+
+                    insertIntoHOStmt.executeUpdate();
+                    insertIntoHOStmt.close();
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
+            }
+        };
 
+        channel.basicConsume(BO1_QUEUE_NAME, true, consumer_1);
+        channel.basicConsume(BO2_QUEUE_NAME, true, consumer_2);
 
-        }, consumerTag -> {});
+        // Keep main thread alive
+        /*while (true) {
+            Thread.sleep(1000);
+        }*/
     }
+
 
 
         public void connect() throws Exception {
@@ -85,15 +137,16 @@ public class HOController {
             // Connect to MySQL database
             // Set up MySQL database connection
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection dbConnection = (Connection) DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/ho", "root", "");
-            insertIntoHOStmt = ((java.sql.Connection) dbConnection).prepareStatement("INSERT INTO `product sales` (date, region, product, qty, cost, amt, tax, total) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+            this.dbconnection  = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            insertIntoHOStmt = this.dbconnection.prepareStatement("INSERT INTO `product sales` (date, region, product, qty, cost, amt, tax, total) VALUES (?,?,?,?,?,?,?,?)");
+
+
         }
 
 
-    public void insertSale(String region, String product, int qty, float cost, double amt, float tax, double total) throws SQLException {
+    public void insertSale(Date date ,String region, String product, int qty, float cost, double amt, float tax, double total) throws SQLException {
         // Insert sale into HO database
-        insertIntoHOStmt.setDate(1, new Date(System.currentTimeMillis()));
+        insertIntoHOStmt.setDate(1, date );
         insertIntoHOStmt.setString(2, region);
         insertIntoHOStmt.setString(3, product);
         insertIntoHOStmt.setInt(4, qty);
@@ -106,6 +159,6 @@ public class HOController {
 
 
     public void close() throws Exception {
-        rabbitmqConnection.close();
+        dbconnection.close();
     }
 }
